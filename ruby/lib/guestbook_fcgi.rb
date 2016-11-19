2c36c9c7a45b2c96df07cac02587db3691b0c7c8
Vasyl Lytovchenko <muaddeep@gmail.com>
2016-11-19 12:21:10 +0200

    moving static text to functions

--- ruby/lib/guestbook_fcgi.rb
+++ ruby/lib/guestbook_fcgi.rb
@@ -7,7 +7,7 @@ require 'csv'
 require 'json'
 
 class GuestbookApp
-    def initialize 
+    def initialize
         @guest_records_path = '/srv/www/mvcdemo.onpu/private/guest_records.csv'
         @guest_records_mandatory_columns = [:name, :email, :city, :country, :comments ]
         @guest_records_optional_columns = [:state, :url]
@@ -77,8 +77,25 @@ class GuestbookApp
         end
         records += "</div>"
 
-        ###### header
-        head = '
+        saved_form_values = "<script>";
+        saved_form_values += 'saved_form_values = new Map(' + JSON.generate(params.map{|k,v| [k,v]}) + ');';
+        saved_form_values += '
+    function restoreValues(value, key, map){
+        document.getElementById(key).setAttribute("value", value);
+    }
+    saved_form_values.forEach(restoreValues);
+        '
+        saved_form_values += "</script>"
+
+        ##### output everything
+        response =  "#{head}<html><body>#{ errors if have_errors}#{form}#{records}#{footer}#{saved_form_values}</body></html>"
+        [200, {"Content-Type" => "text/html"}, [response]]
+    end
+
+    private
+
+    def head
+        '
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -96,87 +113,76 @@ class GuestbookApp
     </style>
 </head>
         '
-        header = ''
 
-        ##### footer
-        footer = '<h6> (c) 2016, Vasyl Lytovchenko, mvcdemo.onpu</h6>'
+    end
 
-        ##### form
-        form = '
-<div class="container">
-    <div class="">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title">add to our guestbook</h3>
-                Fill in the blanks below to add to our guestbook. The only blanks that you have to fill in are the comments and name section. Thanks!</div>
-            <div class="panel-body">
-                <form method="POST" class="form-horizontal">
-                    <div class="form-group">
-                        <label for="name" class="control-label col-sm-2 required">Name:</label>
-                        <div class="col-sm-8">
-                            <input type = "text" class="form-control" id="name" name="name" required="true">
+    def footer
+        "<h6> (c) 2016, Vasyl Lytovchenko, mvcdemo.onpu</h6>"
+    end
+
+    def form
+        '
+            <div class="container">
+                <div class="">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">add to our guestbook</h3>
+                            Fill in the blanks below to add to our guestbook. The only blanks that you have to fill in are the comments and name section. Thanks!</div>
+                        <div class="panel-body">
+                            <form method="POST" class="form-horizontal">
+                                <div class="form-group">
+                                    <label for="name" class="control-label col-sm-2 required">Name:</label>
+                                    <div class="col-sm-8">
+                                        <input type = "text" class="form-control" id="name" name="name" required="true">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="email" class="control-label col-sm-2 required">E-Mail:</label>
+                                    <div class="col-sm-8">
+                                        <input type = "email" class="form-control" id="email" name="email" required="true">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="url" class="control-label col-sm-2">URL: </label>
+                                    <div class="col-sm-8">
+                                        <input type = "url" class="form-control" id="url" name="url">
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-12 form-group">
+                                    <label for="city" class="control-label col-sm-2 required" required>City: </label>
+                                    <div class="col-sm-2">
+                                        <input type = "text" class="form-control" id="city" name="city" size="10" required="true">
+                                    </div>
+
+                                    <label for="state" class="control-label col-sm-1">State: </label>
+                                    <div class="col-sm-1">
+                                        <input type = "text" class="form-control" id="state" name="state" size="2">
+                                    </div>
+
+                                    <label for="country" class="control-label col-sm-2 required">Country: </label>
+                                    <div class="col-sm-2">
+                                        <input type = "text" class="form-control" id="country" name = "country" size="15" required="true">
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-10">
+                                    <div class="form-group">
+                                        <label for="comments" class="control-label col-sm-3 required">Comments: </label>
+                                        <div class="col-sm-8">
+                                            <textarea class="form-control" id="comments" name="comments"></textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-10">
+                                    <button type = "reset" class="btn btn-danger"> Reset </button>
+                                    <button type = "submit" class="btn btn-primary"> Submit </button>
+                                </div>
+                            </form>
                         </div>
                     </div>
-                    <div class="form-group">
-                        <label for="email" class="control-label col-sm-2 required">E-Mail:</label>
-                        <div class="col-sm-8">
-                            <input type = "email" class="form-control" id="email" name="email" required="true">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="url" class="control-label col-sm-2">URL: </label>
-                        <div class="col-sm-8">
-                            <input type = "url" class="form-control" id="url" name="url">
-                        </div>
-                    </div>
-
-                    <div class="col-sm-12 form-group">
-                        <label for="city" class="control-label col-sm-2 required" required>City: </label>
-                        <div class="col-sm-2">
-                            <input type = "text" class="form-control" id="city" name="city" size="10" required="true">
-                        </div>
-
-                        <label for="state" class="control-label col-sm-1">State: </label>
-                        <div class="col-sm-1">
-                            <input type = "text" class="form-control" id="state" name="state" size="2">
-                        </div>
-
-                        <label for="country" class="control-label col-sm-2 required">Country: </label>
-                        <div class="col-sm-2">
-                            <input type = "text" class="form-control" id="country" name = "country" size="15" required="true">
-                        </div>
-                    </div>
-
-                    <div class="col-sm-10">
-                        <div class="form-group">
-                            <label for="comments" class="control-label col-sm-3 required">Comments: </label>
-                            <div class="col-sm-8">
-                                <textarea class="form-control" id="comments" name="comments"></textarea>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-10">
-                        <button type = "reset" class="btn btn-danger"> Reset </button>
-                        <button type = "submit" class="btn btn-primary"> Submit </button>
-                    </div>
-                </form>
+                </div>
             </div>
-        </div>
-    </div>
-</div>
         '
-        saved_form_values = "<script>";
-        saved_form_values += 'saved_form_values = new Map(' + JSON.generate(params.map{|k,v| [k,v]}) + ');';
-        saved_form_values += '
-    function restoreValues(value, key, map){
-        document.getElementById(key).setAttribute("value", value);
-    }
-    saved_form_values.forEach(restoreValues);
-        '
-        saved_form_values += "</script>"
-
-        ##### output everything
-        response =  "#{head}<html><body>#{header}#{ errors if have_errors}#{form}#{records}#{footer}#{saved_form_values}</body></html>"
-        [200, {"Content-Type" => "text/html"}, [response]]
     end
 end
