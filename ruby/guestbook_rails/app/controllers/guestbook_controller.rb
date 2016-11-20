cbcc0dd99ed1e92ded3b9d638c899d346f34c525
Vasyl Lytovchenko <muaddeep@gmail.com>
2016-11-20 02:35:28 +0200

    now creating values

--- ruby/guestbook_rails/app/controllers/guestbook_controller.rb
+++ ruby/guestbook_rails/app/controllers/guestbook_controller.rb
@@ -2,4 +2,14 @@ class GuestbookController < ApplicationController
    def index
        @records = Record.all
    end
+
+   def create
+       record = Record.create(record_params)
+       redirect_to action: 'index'
+   end
+
+   private
+   def record_params
+       params.require(:record).permit(:name, :email, :city, :country, :comments, :state, :url)
+   end
 end
--- ruby/guestbook_rails/config/routes.rb
+++ ruby/guestbook_rails/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     get '/guestbook' => 'guestbook#index'
+    post '/guestbook' => 'guestbook#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
